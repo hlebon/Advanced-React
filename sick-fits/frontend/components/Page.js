92715748391c963d502a080d5a13821f2019ae7a
hanslg <hans@wisy.app>
2019-08-23 00:34:03 -0400

    feat: meta component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from "./Header";
+import Meta from "./Meta";
 
 const Page = (props) => {
   return (
     <div>
+      <Meta/>
       <Header/>
       {props.children}
     </div>
