63c0facae2f922b12bed7076191be6ff8fce28e7
hanslg <hans@wisy.app>
2019-09-03 00:49:59 -0400

    creating theme

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Header from "./Header";
 import Meta from "./Meta";
+import styled from "styled-components";
+
+const theme = {
+  red: "#FF0000",
+  back: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "",
+  offwhite: ""
+}
+
+const StyledPage = styled.div`
+  background: #fff;
+  color: back;
+`
+
+const Inner = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 2rem;
+`
 
 const Page = (props) => {
   return (
-    <div>
+    <StyledPage>
       <Meta/>
       <Header/>
-      {props.children}
-    </div>
+      <Inner>
+        {props.children}
+      </Inner>
+    </StyledPage>
    );
 }
 
